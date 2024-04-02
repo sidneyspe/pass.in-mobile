5d6eb83a54c663629c36924d55416283afcd8312
Sidney Pimentel <sidneyspe@gmail.com>
2024-04-02 17:31:36 -0300

    updated project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,29 @@
-import "@/src/styles/global.css"
+import "@/styles/global.css"
 
 import { Slot } from 'expo-router'
-import { StatusBar } from "expo-status-bar"
+
+import { Loading } from "@/components/loading";
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+  Roboto_500Medium
+} from '@expo-google-fonts/roboto'
+import { StatusBar } from "react-native";
 
 export default function Layout() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+
   return (
     <>
-      <StatusBar style="light" />
-      <Slot />
+      <StatusBar barStyle="light-content" />
+      {fontsLoaded ? <Slot /> : <Loading />}
     </>
   )
 }
